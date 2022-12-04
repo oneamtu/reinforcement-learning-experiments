bce97235d305e53c8651c32f1cdb315776e20214
Octavian Neamțu <28197+oneamtu@users.noreply.github.com>
2022-12-04 12:15:10 +0000

    Implement neural net.

--- semi-gradient-td-function-approximation-tc-nn/nn.py
+++ semi-gradient-td-function-approximation-tc-nn/nn.py
@@ -1,21 +1,44 @@
 import numpy as np
 from algo import ValueFunctionWithApproximation
 
-import tensorflow as tf
+import torch
+import torch.nn as nn
+from torch import optim
 
-class ValueFunctionWithNN(ValueFunctionWithApproximation):
-    def __init__(self,
-                 state_dims):
-        """
-        state_dims: the number of dimensions of state space
-        """
-        # TODO: implement this method
+class ValueFunctionWithNN:
+    def __init__(self, state_dims, hidden_units=32):
+        # Initialize the neural network model
+        self.model = nn.Sequential(
+          nn.Linear(state_dims, hidden_units),
+          nn.ReLU(),
+          nn.Linear(hidden_units, hidden_units),
+          nn.ReLU(),
+          nn.Linear(hidden_units, 1),
+        )
 
-    def __call__(self,s):
-        # TODO: implement this method
-        return 0.
+        # Compile the model with the AdamOptimizer
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001, betas=(0.9, 0.999))
 
-    def update(self,alpha,G,s_tau):
-        # TODO: implement this method
-        return None
+    def __call__(self, s) -> float:
+        # Use the model to predict the value of the given state
+        self.model.eval()
+        input = torch.from_numpy(s).float()
+        return self.model(input)[0].item()
 
+    def update(self, alpha, G, s_tau):
+        # Calculate the predicted value of the state at the time step where the return was calculated
+        self.model.train()
+
+        input = torch.from_numpy(s_tau).float()
+        predicted_value = self.model(input)
+
+        # Compute the loss between the predicted and target values using the MSE loss
+        loss = 0.5*nn.MSELoss()(predicted_value, torch.tensor(G))
+
+        # Use the Adam optimizer to update the parameters of the neural network based on the computed loss
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # Return the loss value
+        return loss.item()
