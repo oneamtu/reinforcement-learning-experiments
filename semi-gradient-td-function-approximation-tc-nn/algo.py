23ad8137293d3563124e40105e9b13bd47a4c3b7
Octavian Neamțu <28197+oneamtu@users.noreply.github.com>
2022-12-04 12:13:40 +0000

    Remove TODO.

--- semi-gradient-td-function-approximation-tc-nn/algo.py
+++ semi-gradient-td-function-approximation-tc-nn/algo.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pickle
+import os
 from policy import Policy
 
 class ValueFunctionWithApproximation(object):
@@ -50,5 +52,47 @@ def semi_gradient_n_step_td(
     output:
         None
     """
-    #TODO: implement this function
+    trajs = []
 
+    cached_path = f'trajs.pkl'
+    if os.path.exists(cached_path):
+        trajs = pickle.load(open(cached_path, 'rb'))
+    else:
+        for episode in range(num_episode):
+            states, actions, rewards, done = [env.reset()], [], [], False
+            # env.render()
+
+            while not done:
+                action = pi.action(states[-1])
+                state, reward, done, _ = env.step(action)
+                # env.render()
+
+                states.append(state)
+                actions.append(action)
+                rewards.append(reward)
+
+            traj = list(zip(states[:-1],actions,rewards,states[1:]))
+            trajs.append(traj)
+
+        pickle.dump(trajs, open(cached_path, 'wb'))
+
+    # pre-compute gammas power series
+    gammas = np.power(gamma, range(n+1))
+
+    for i, episode in enumerate(trajs):
+        T = len(episode)
+        loss = 0
+        print(f'Episode {i}')
+        for tau in range(T):
+            (state_tau, _, _, _) = episode[tau]
+
+            len_mc = min(tau + n, T) - tau
+            # TODO: this could be O(1) rather than O(n)
+            rewards = np.array([reward for (_, _, reward, _) in episode[tau:tau+len_mc]])
+            G = np.sum(gammas[:len_mc] * rewards)
+            if tau + n < T:
+                G += gammas[len_mc] * V(episode[tau+n][0])
+            loss += V.update(alpha, G, state_tau)
+        print(f'Loss {loss}')
+
+    return V
